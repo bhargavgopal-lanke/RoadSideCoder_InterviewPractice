79b58b07341b2735d532c68518d6670d88819b3e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-07-29 21:33:17 +0530

    feat: enhance StarRating component with hover effect and state management

--- Posts-Task/posts/src/Components/StarRating.jsx
+++ Posts-Task/posts/src/Components/StarRating.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const StarRating = ({ size = 5, rating, onChange }) => {
-  const starClass = "star";
+const StarRating = ({ size = 5, rating, onChange = () => {} }) => {
+  const [hoverRating, setHoverRating] = useState(0);
+  const handleMouseHover = (starValue) => {
+    setHoverRating(starValue);
+  };
   return (
     <div>
-      <div>
-        {Array(size)
-          .fill("")
-          .map((_, i) => {
-            return (
-              <span key={i} className={starClass}>
-                ⭐
-              </span>
-            );
-          })}
-      </div>
+      {Array(size)
+        .fill("")
+        .map((_, index) => {
+          let starClass = "star";
+          const starValue = index + 1;
+
+          if (hoverRating >= starValue) {
+            starClass += " hover";
+          } else if (rating >= starValue) {
+            starClass += " active";
+          }
+
+          return (
+            <span
+              key={index}
+              className={starClass}
+              onClick ={() => onChange(starValue)}
+              onMouseEnter={() => handleMouseHover(starValue)}
+              onMouseLeave={() => handleMouseHover(0)}
+            >
+              &#9733;
+            </span>
+          );
+        })}
     </div>
   );
 };
