a230c1ef1c2eea1c986ca6443c87bd14c411c157
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-07 21:20:09 +0530

    add tests little bit complex. but i have to push harder myself.

--- trafficsignalreactapp/src/__tests__/App.test.tsx
+++ trafficsignalreactapp/src/__tests__/App.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
+
+
 describe("App component test", () => {
   test("the component should render", () => {
     const component = <App />;
--- trafficsignalreactapp/src/__tests__/TrafficLights.test.tsx
+++ trafficsignalreactapp/src/__tests__/TrafficLights.test.tsx
@@ -1,10 +1,16 @@
-import { render } from "@testing-library/react";
-import TrafficLights from "../Components/TrafficLights";
-import { trafficLightsObj } from "../utils/util";
+import { setUpTrafficLightsTest } from "../utils/setupTrafficLightsTest";
+import "@testing-library/jest-dom";
 
 describe("Traffic Light components tests", () => {
-  test("should check the component render", () => {
-    render(<TrafficLights lights={trafficLightsObj} />);
-    // expect()
+  test("should renders initial green light", () => {
+    const { getLightByColor } = setUpTrafficLightsTest();
+    expect(getLightByColor("green")).toBeTruthy();
+  });
+
+  test("clears timeout on unmount", () => {
+    const clearSpy = jest.spyOn(global, "clearTimeout");
+    const { unmount } = setUpTrafficLightsTest();
+    unmount();
+    expect(clearSpy).toHaveBeenCalled();
   });
 });
