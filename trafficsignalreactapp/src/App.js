6a012b8de0526926dfeb96565f11ec1df0f19cf3
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-07 09:47:05 +0530

    UI created for traffic signal apps.

--- trafficsignalreactapp/src/App.js
+++ trafficsignalreactapp/src/App.js
@@ -1,10 +1,50 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentLight, setCurrentLight] = useState("green");
+
+  const trafficLights = {
+    green: {
+      color: "green",
+      duration: 4000,
+      next: "yellow",
+    },
+    yellow: {
+      color: "yellow",
+      duration: 2000,
+      next: "red",
+    },
+    red: {
+      color: "red",
+      duration: 5000,
+      next: "green",
+    },
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {});
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  console.log(trafficLights[currentLight]);
+
   return (
-    <div>
-      Traffic App
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}> Traffic App</h1>
+      <div className="signal-lights-main">
+        {Object.keys(trafficLights)?.map((x, i) => {
+          return (
+            <div
+              className="traffic-lights"
+              key={i}
+              style={{ backgroundColor: x }}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
