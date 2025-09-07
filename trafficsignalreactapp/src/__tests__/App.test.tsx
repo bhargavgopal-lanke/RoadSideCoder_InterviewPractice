f8bb5caaa71d5910b293ac6b94ae1405b230e385
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-07 14:24:04 +0530

    coverage issue fixed.

--- trafficsignalreactapp/src/__tests__/App.test.tsx
+++ trafficsignalreactapp/src/__tests__/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
-
-test("the component should render", () => {
-  const component = <App />;
-  render(component);
-  const heading = screen.getByText("Traffic App");
-  expect(heading).toBeInTheDocument();
+describe("App component test", () => {
+  test("the component should render", () => {
+    const component = <App />;
+    render(component);
+    const heading = screen.getByText("Traffic App");
+    expect(heading).toBeInTheDocument();
+  });
 });
