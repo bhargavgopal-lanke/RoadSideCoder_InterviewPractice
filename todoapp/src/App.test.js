e9a4e9c441d1ae1e8466f8d14f8bfeee1baad8c7
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-08 15:18:20 +0530

    add test case for handlechnage event.

--- todoapp/src/App.test.js
+++ todoapp/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders App component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const submitElement = screen.getByText("Submit");
+  expect(submitElement).toBeInTheDocument();
+});
+
+test("calls handlechange and updates input value", () => {
+  render(<App />);
+  const input = screen.getByRole("textbox");
+  fireEvent.change(input, { target: { value: "Bhargav" } });
+  expect(input).toHaveValue("Bhargav");
 });
