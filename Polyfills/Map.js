bbd185d7fd528400e9481399d8ee70670dd200db
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-11 16:54:41 +0530

    polyfill filter method code.

--- Polyfills/Map.js
+++ Polyfills/Map.js
@@ -9,3 +9,29 @@ Array.prototype.myMap = function (cb) {
 
   return temp;
 };
+
+const nums = [1, 2, 3, 4];
+
+const mutliplyByThree = nums.myMap((num, i, arr) => {
+  return num * 3;
+});
+
+console.log("polyfill output", mutliplyByThree);
+
+// polyfill for filter
+
+Array.prototype.filter = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      temp.push(this[i]);
+    }
+  }
+  return temp;
+};
+
+const numGreaterThanTwo = nums.filter((num, i, arr) => {
+  return num > 2;
+});
+
+console.log("polyfill filter result", numGreaterThanTwo);
