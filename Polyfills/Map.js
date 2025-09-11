c93e42f41dce3953d1fed409d032f68d49c776c6
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-09-11 16:46:12 +0530

    Polyfill created for map method.

--- Polyfills/Map.js
+++ Polyfills/Map.js
@@ -1,16 +1,11 @@
-const arr = [1, 2, 3, 4];
+// custom map function created using polyfills
+// it works in older version of browsers
 
-const mapRes = arr.map((x) => x * 3);
+Array.prototype.myMap = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    temp.push(cb(this[i], i, this));
+  }
 
-console.log("mapRes", mapRes);
-
-const filterRes = arr.filter((x) => x > 2);
-
-console.log("filterRes", filterRes);
-
-// the value return by callback function in each iteration will becomes the new accumulator
-const reduceRes = arr.reduce((acc, curr) => {
-  return acc + curr;
-}, 0);
-
-console.log("reduceRes", reduceRes);
+  return temp;
+};
